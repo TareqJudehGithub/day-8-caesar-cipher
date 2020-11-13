b5daf716f055104919a59962849ba6219b144c1d
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-13 16:16:33 +0000

    Refactored code to just include one function for both encrypting and decrypting.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -6,42 +8,17 @@ shift = int(input("Type the shift number:\n"))
 
 #Don't change the code above 👆
 
-# Encrypting:
-
-# Creating a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
-    
-    cipher_text = ""
-    # Figuring out the index of each letter in plain_text:
-    for letter in plain_text:
-      position = alphabet.index(letter)
-      # Shifting each letter index to their new position:
-      new_position = position + shift_amount
-      # New letters:
-      new_letter = alphabet[new_position]
-      # Coded text:
-      cipher_text += new_letter
-    print(cipher_text)
-  
-
-# Decrypting:
-
-# Creating a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-def decrypt(plain_text, shift_amount):
- 
-  word = ""
+def ceaser(plain_text, shift_amount, cipher_direction):
+  end_text = ""
   for letter in plain_text:
     position = alphabet.index(letter)
-    new_position = position - shift_amount
+    if cipher_direction == "encode":
+      new_position = position + shift_amount
+    elif cipher_direction == "decode":
+      new_position = position - shift_amount
     new_letter = alphabet[new_position]
-    word += new_letter
-  print(word)
+    end_text += new_letter
+  print(f"The {cipher_direction}d text is: {end_text}")
 
 
-# Calling functions based on direction variable input entry:
-if direction == "encode":
-  encrypt(text, shift)
-elif direction == "decode":
-  decrypt(text, shift)
-else: 
-  print("Bad entry.")
+ceaser(text, shift, direction)
