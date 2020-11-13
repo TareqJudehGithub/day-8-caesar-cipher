28e75b6c71e11fc80b40a7181009379b4c46f2d4
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-13 23:46:22 +0000

    Added a while loop, prompting user if they want to try Caeser Cipher again or quit.

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
+from art import logo
+from replit import clear
+restart = True
+while restart:
+
+  print(logo)
+  print('''
+  ''')
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  #Don't change the code above 👆
+
+  def caeser(plain_text, shift_amount, cipher_direction):
+    
+    shift_amount = shift_amount % 25
+
+    end_text = ""
+
+    for char in plain_text:      
+      if char in alphabet:
+        position = alphabet.index(char)
+        if cipher_direction == "encode":
+          new_position = position + shift_amount
+        elif cipher_direction == "decode":
+          new_position = position - shift_amount   
+        new_letter = alphabet[new_position]
+        end_text += new_letter
+  
+      else:
+        end_text += char
+   
+    print(f"The {cipher_direction}d text is: {end_text}")
+    print('''
+    ''')
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  caeser(text, shift, direction)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#Don't change the code above 👆
-
-def ceaser(plain_text, shift_amount, cipher_direction):
-  end_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    if cipher_direction == "encode":
-      new_position = position + shift_amount
-    elif cipher_direction == "decode":
-      new_position = position - shift_amount
-    new_letter = alphabet[new_position]
-    end_text += new_letter
-  print(f"The {cipher_direction}d text is: {end_text}")
-
-
-ceaser(text, shift, direction)
+  restart_ceaser = input("Would you like to cipher another word? (y/n) ").lower()
+  if restart_ceaser == "y":
+    clear()
+    continue
+  else:
+    print("Good Bye!")
+    break
